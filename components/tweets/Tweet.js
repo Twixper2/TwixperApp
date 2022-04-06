a84ed796a17f725d1163aa3bf6576fb05287f023
DanielDinshtein <danieldinsh@gmail.com>
2022-04-06 12:30:15 +0300

    Before try Clone 1

--- components/tweets/Tweet.js
+++ components/tweets/Tweet.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
 const Tweet = (props) => {
 	const { tweetData } = props;
@@ -12,7 +13,13 @@ const Tweet = (props) => {
 						<Image source={{ uri: tweetData.author.profileImgUrl }} style={styles.userImage} />
 					</View>
 				</View>
-				<View style={styles.postContent}></View>
+				<View style={styles.postContent}>
+					<View style={styles.postUserInfo}>
+						<Text style={styles.textH4}>
+							{tweetData.author.userFullName} { tweetData.author.isVerified && <AntDesign name="checkcircle" size={18} style={styles.checkCircle} />}
+						</Text>
+					</View>
+				</View>
 			</View>
 		</View>
 	);
@@ -24,7 +31,7 @@ const styles = StyleSheet.create({
 	},
 	postBorder: {
 		display: "flex",
-		flexDirection: "column",
+		flexDirection: "row",
 		paddingVertical: 16,
 		paddingRight: 0,
 		paddingLeft: 16,
@@ -34,20 +41,42 @@ const styles = StyleSheet.create({
 	userAvatarContainer: {
 		width: "100%",
 		height: "100%",
-		flex: 12,
+		flex: 2,
 	},
 	userAvatar: {
-		width: 80,
-		height: 80,
+		width: 60,
+		height: 60,
 	},
 	userImage: {
-		width: 80,
-		height: 80,
+		width: 60,
+		height: 60,
 		resizeMode: "contain",
-		borderRadius: 50
+		borderRadius: 50,
 	},
 	postContent: {
-		flex: 86.5,
+		flex: 8,
+		width: "100%",
+	},
+	postUserInfo: {
+		// display: "flex",
+		// justifyContent: "center",
+		// alignItems: "center",
+		marginVertical: 8,
+		marginHorizontal: 0,
+		maxWidth: "95%",
+	},
+	textH4: {
+		maxWidth: "74%",
+		fontSize: 18,
+		fontWeight: "bold",
+		justifyContent: "center",
+		alignContent: "center",
+		alignItems: "center",
+		marginRight: 8,
+	},
+	checkCircle: {
+		marginRight: 8,
+		color: "#1aa1f5",
 	},
 });
 
