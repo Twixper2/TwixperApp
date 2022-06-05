e1211176f34e076546564b3ba5ce111390f5e4d6
danieldinshtein <danieldinsh@gmail.com>
2022-06-05 18:41:19 +0300

    Lecture 250 - Reacting to User Iteraction with Incoming Notifications

--- screens/PushNotifications.js
+++ screens/PushNotifications.js
@@ -14,7 +14,7 @@ Notifications.setNotificationHandler({
 
 const PushNotifications = () => {
 	useEffect(() => {
-		const subscription = Notifications.addNotificationReceivedListener((notification) => {
+		const subscription1 = Notifications.addNotificationReceivedListener((notification) => {
 			console.log("NOTIFICATION RECEIVED");
 			console.log(notification);
 			const userName = notification.request.content.data.userName;
@@ -22,8 +22,17 @@ const PushNotifications = () => {
 			console.log(userName);
 		});
 
+		const subscription2 = Notifications.addNotificationResponseReceivedListener((response) => {
+			console.log("NOTIFICATION RESPONSE RECEIVED");
+			console.log(response);
+			const userName = response.notification.request.content.data.userName;
+
+			console.log(userName);
+		});
+
 		return () => {
-			subscription.remove();
+			subscription1.remove();
+			subscription2.remove();
 		};
 	}, []);
 
