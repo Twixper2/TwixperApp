8d6764ba8a607de403bc595d0c86ffc333466460
danieldinshtein <danield@blinkops.com>
2022-06-09 23:14:26 +0300

    Add getPersonsData Dummy func

--- components/people/PersonPreview.js
+++ components/people/PersonPreview.js
@@ -1,8 +1,29 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View, Text, TouchableHighlight } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import EvilIcons from "react-native-vector-icons/EvilIcons";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+import ProfileImage from "../UI/ProfileImage";
 
 import { appColors } from "../../constants/colors";
+import { PROFILE_SCREEN, TWEET_SCREEN } from "../../constants/screenNames";
 
-const PersonPreview = () => {
+import { getPersonData } from "../../utils/storageFunctions";
+
+const PersonPreview = (props) => {
+	const navigation = useNavigation();
+	const [touched, setTouched] = useState(false);
+
+	// const { personData } = props;
+	const personData = getPersonData()[0]
+	const { username, userHandle, profileImgURL, userDescription, FollowingStatus } = personData;
+
+	console.log(personData)
+	console.log(username, userHandle, profileImgURL, userDescription, FollowingStatus)
+
+
 	return (
 		<View style={styles.container}>
 			<Text style={styles.tempText}>This is :{"\n"}PersonPreview !!</Text>
