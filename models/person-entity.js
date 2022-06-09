d709427e328f3d6a360f628524efc9c87e472ee7
danieldinshtein <danieldinsh@gmail.com>
2022-06-09 21:33:23 +0300

    PersonEntity

---
+++ models/person-entity.js
@@ -0,0 +1,11 @@
+class PersonEntity {
+	constructor(username, userHandle, profileImgURL, userDescription, FollowingStatus) {
+		this.username = username;
+		this.userHandle = userHandle;
+		this.profileImgURL = profileImgURL;
+		this.userDescription = userDescription;
+		this.FollowingStatus = FollowingStatus;
+	}
+}
+
+export default PersonEntity;
