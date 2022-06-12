0817af13e0751831b8fbe8eb21f8c58c79fc8469
danieldinshtein <danield@blinkops.com>
2022-06-12 23:45:41 +0300

    useState in ExpandableImg try

--- components/UI/ExpandableImg.js
+++ components/UI/ExpandableImg.js
@@ -1,21 +1,62 @@
 import { TouchableOpacity, Image, Dimensions } from "react-native";
+import { useState } from "react";
 
 const ExpandableImg = ({ onPress, mediaData, pixelMedia }) => {
 	const { media_url_https, sizes } = mediaData;
 
 	const urlParams = new URLSearchParams(media_url_https);
-
 	const imageSize = urlParams.get("name");
 
-	const imageStyle = {
+	const normalImgSize = {
 		height: sizes[imageSize].h,
 		width: sizes[imageSize].w,
 		// resizeMode: sizes[imageSize].resize,
 		resizeMode: "center",
 	};
 
+	const explantedImgSize = {
+		height: Dimensions.get("window").height,
+		width: Dimensions.get("window").width,
+		// resizeMode: sizes[imageSize].resize,
+		resizeMode: "cover",
+	};
+
+	const normalContainerStyle = {
+		flex: 1,
+	};
+
+	const explantedContainerStyle = {
+		flex: 1,
+		height: "100%",
+		width: "100%",
+		alignItems: "stretch",
+	};
+
+	const [imageStyle, setImageStyle] = useState(normalImgSize);
+	const [containerStyle, setContainerStyle] = useState(normalContainerStyle);
+	const [isExpanded, setIsExpanded] = useState(false);
+
+	const onImagePressHandler = () => {
+		if (isExpanded) {
+			setImageStyle(normalImgSize);
+			setContainerStyle(normalContainerStyle);
+			setIsExpanded(false);
+		} else {
+			setImageStyle(explantedImgSize);
+			setContainerStyle(explantedContainerStyle);
+			setIsExpanded(true);
+		}
+	};
+
+	// const imageStyle = {
+	// 	height: sizes[imageSize].h,
+	// 	width: sizes[imageSize].w,
+	// 	// resizeMode: sizes[imageSize].resize,
+	// 	resizeMode: "center",
+	// };
+
 	return (
-		<TouchableOpacity onPress={onPress}>
+		<TouchableOpacity onPress={onImagePressHandler} style={containerStyle}>
 			<Image style={imageStyle} source={{ uri: media_url_https }} blurRadius={pixelMedia ? 7 : undefined} />
 		</TouchableOpacity>
 	);
